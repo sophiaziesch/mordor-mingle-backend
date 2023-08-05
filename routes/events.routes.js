761ebe19b8ecd38bc32cdcde4f801c3ca2b9f62a
Sophia Ziesch <s.ziesch@posteo.de>
2023-08-05 12:03:22 +0200

    events routes done

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
 		const events = await Event.find();
 		res.status(200).json(events);
 	} catch (error) {
-		console.log("Error in GET all events: ", error);
+		console.log("Error on GET all events: ", error);
 		res.status(500).json(error);
 	}
 });
@@ -18,15 +18,48 @@ router.get("/:eventId", async (req, res) => {
 		const event = await Event.findById(req.params.eventId);
 		res.status(200).json(event);
 	} catch (error) {
-		console.log("Error in GET on event: ", error);
+		console.log("Error on GET on event: ", error);
 		res.status(500).json(error);
 	}
 });
 
 /* POST one event (create) */
+router.post("/", async (req, res) => {
+	try {
+		const payload = req.body;
+		const newEvent = await Event.create(payload);
+	} catch (error) {
+		console.log("Error on POST one event: ", error);
+		res.status(500).json(error);
+	}
+});
 
 /* PUT one event (update) */
+router.put("/:eventId"),
+	async (req, res) => {
+		const payload = req.body;
+		const updatedEvent = await Event.findByIdAndUpdate(
+			req.params.eventId,
+			payload,
+			{ new: true }
+		);
+		res.status(202).json(updatedEvent);
+		try {
+		} catch (error) {
+			console.log("Error on PUT one event: ", error);
+			res.status(500).json(error);
+		}
+	};
 
 /* DELETE one event (delete) */
+router.delete("/:eventId", async (req, res) => {
+	try {
+		await Event.findByIdAndDelete(req.params.studentId);
+		res.status(202).json({ message: "Event deleted" });
+	} catch (error) {
+		console.log("Error on DELETE one student: ", error);
+		res.status(500).json(error);
+	}
+});
 
 module.exports = router;
