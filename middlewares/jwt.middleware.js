bd5b6f0a1af8d00b44df84685c5e8a9b4a0b43f6
Sophia Ziesch <s.ziesch@posteo.de>
2023-08-06 10:36:43 +0200

    POST login und jwt.middleware done

---
+++ middlewares/jwt.middleware.js
@@ -0,0 +1,25 @@
+const { expressjwt } = require("express-jwt");
+
+/* Instantiate JWT token validation middleware */
+const isAuthenticated = expressjwt({
+	secret: process.env.TOKEN_SECRET,
+	algorithms: ["HS256"],
+	requestProperty: "payload",
+	getToken: getTokenFromHeaders,
+});
+
+/* Function used to extract JWT token from the req's "Authorization" Headers */
+const getTokenFromHeaders = (req) => {
+	/* Check if token is available on req Headers */
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.split(" ")[0] === Bearer
+	) {
+		/* Get encoded token string and return it */
+		const token = req.headers.authorization.split(" ")[1];
+		return token;
+	}
+	return null;
+};
+
+module.exports = { isAuthenticated };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+const { isAuthenticated } = require("../middlewares/jwt.middleware");
 
 router.get("/", (req, res, next) => {
 	res.json("All good in auth");
@@ -21,3 +23,28 @@ router.post("/signup", async (req, res) => {
 });
 
 /* POST route to login */
+router.post("/login", async (req, res) => {
+	const payload = req.body;
+	/* Check if user exists */
+	const potentialUser = await User.findOne({ username: payload.username });
+	if (potentialUser) {
+		if (bcrypt.compareSync(payload.password, potentialUser.password)) {
+			/* Sign the JWT */
+			const authToken = jwt.sign(
+				{ userId: potentialUser._id },
+				process.env.TOKEN_SECRET,
+				{ algorithm: "H256", expiresIn: "6h" }
+			);
+			/* Sending back token to the front */
+			res.status(202).json({ token: authToken });
+		} else {
+			/* Incorrect pasword */
+			res.status(403).json({ errorMessage: "Password invalid" });
+		}
+	} else {
+		/* No user found */
+		res.status(403).json({ errorMessage: "No user found" });
+	}
+});
+
+/* GET route to verify token */
