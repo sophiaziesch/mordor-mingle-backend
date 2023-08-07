5e43856b3183cbfc9b4fd61ab6f823879a145d60
3anza <salmaaldubouni@gmail.com>
2023-08-07 23:43:08 +0200

    user put route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -72,4 +72,30 @@ router.get("/verify", isAuthenticated, async (req, res, next) => {
 	}
 });
 
+/* GET one user route */
+
+router.get("/:userId", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.userId);
+		res.status(200).json(user);
+	} catch (error) {
+		console.log("Error on GET on event: ", error);
+		res.status(500).json(error);
+	}
+});
+
+/* update user info (PUT) */
+router.put("/:userId", async (req, res) => {
+		try {
+			const payload = req.body;
+			const updatedUser = await User.findByIdAndUpdate(req.params.userId, payload, { 
+			new: true 
+		})
+		res.status(202).json(updatedUser);
+		} catch (error) {
+			console.log("Error updating user info: ", error);
+			res.status(500).json(error);
+		} 
+	})
+
 module.exports = router;
