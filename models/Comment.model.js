52f5ec904cad0593328742cd7999d1e4fdd8e5a5
Alcaraz <jaime.mediano@gmail.com>
2023-08-09 17:09:26 +0200

    All working fine

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -1,22 +1,22 @@
 const { Schema, model, SchemaType } = require("mongoose");
 
 const commentSchema = new Schema(
-	{
-		text: {
-			type: String,
-			required: [true, "Text is required."],
-			trim: true,
-		},
-		event: {
-			type: Schema.Types.ObjectId,
-			ref: "Event",
-		},
-		userId: [{ type: Schema.Types.ObjectId, ref: "User" }],
-	},
-	{
-		// this second object adds extra properties: `createdAt` and `updatedAt`
-		timestamps: true,
-	}
+  {
+    text: {
+      type: String,
+      required: [true, "Text is required."],
+      trim: true,
+    },
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+    },
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
 );
 
 const Comment = model("Comment", commentSchema);
--- models/Event.model.js
+++ models/Event.model.js
@@ -1,43 +1,43 @@
 const { Schema, model } = require("mongoose");
 
 const eventSchema = new Schema(
-	{
-		title: {
-			type: String,
-			required: [true, "Title is required."],
-			trim: true,
-		},
-		description: {
-			type: String,
-			required: [true, "Description is required."],
-		},
-		date: {
-			type: Date,
-			default: Date.now,
-			required: [true, "Date is required."],
-		},
-		location: {
-			type: [String],
-			enum: [
-				"Edoras",
-				"Fangorn",
-				"Gondor",
-				"Isengard",
-				"Minas Tirith",
-				"Mordor",
-				"Moria",
-				"Rivendell",
-				"Rohan",
-				"The Shire",
-			],
-		},
-		userId: [{ type: Schema.Types.ObjectId, ref: "User" }],
-		comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-	},
-	{
-		// this second object adds extra properties: `createdAt` and `updatedAt`
-		timestamps: true,
-	}
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required."],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required."],
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+      required: [true, "Date is required."],
+    },
+    location: {
+      type: [String],
+      enum: [
+        "Edoras",
+        "Fangorn",
+        "Gondor",
+        "Isengard",
+        "Minas Tirith",
+        "Mordor",
+        "Moria",
+        "Rivendell",
+        "Rohan",
+        "The Shire",
+      ],
+    },
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
 );
 
 const Event = model("Event", eventSchema);
